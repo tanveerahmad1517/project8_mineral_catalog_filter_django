b7f1d4987a4e0a0c5bafee034ee575b318365fbd
vasilty <vasilty@gmail.com>
2016-07-02 17:51:55 +0200

    Added functionality to the search bar.

--- mineral_catalog/minerals/urls.py
+++ mineral_catalog/minerals/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.mineral_list, name='list'),
     url(r'(?P<pk>\d+)/$', views.mineral_detail, name='detail'),
+    url(r'search/$', views.search, name='search'),
     url(r'(?P<first_letter>[a-z])/$', views.mineral_startswith,
         name='startswith'),
 ]
--- mineral_catalog/minerals/views.py
+++ mineral_catalog/minerals/views.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from django.db.models import Q
 from django.db.models.functions import Lower
 from django.shortcuts import get_object_or_404, render
 
@@ -45,4 +46,29 @@ def mineral_detail(request, pk):
 
 def mineral_startswith(request, first_letter):
     minerals = Mineral.objects.filter(name__startswith=first_letter)
+    return render(request, 'minerals/index.html', {'minerals': minerals})
+
+
+def search(request):
+    term = request.GET.get('q')
+    minerals = Mineral.objects.filter(
+        Q(name__icontains=term)|
+        Q(image_caption__icontains=term) |
+        Q(category__icontains=term) |
+        Q(formula__icontains=term) |
+        Q(strunz_classification__icontains=term) |
+        Q(crystal_system__icontains=term) |
+        Q(unit_cell__icontains=term) |
+        Q(color__icontains=term) |
+        Q(crystal_symmetry__icontains=term) |
+        Q(cleavage__icontains=term) |
+        Q(mohs_scale_hardness__icontains=term) |
+        Q(luster__icontains=term) |
+        Q(streak__icontains=term) |
+        Q(diaphaneity__icontains=term) |
+        Q(optical_properties__icontains=term) |
+        Q(refractive_index__icontains=term) |
+        Q(crystal_habit__icontains=term) |
+        Q(specific_gravity__icontains=term)
+    )
     return render(request, 'minerals/index.html', {'minerals': minerals})
